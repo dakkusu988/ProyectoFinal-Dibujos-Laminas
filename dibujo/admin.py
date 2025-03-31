a26559a4a7f8c659998c673bba0b624359206f9a
David <xdcamaba149@ieshnosmachado.org>
2025-03-31 10:33:18 +0200

    modificado el admin.py para que no tengo errores al añadir Laminas desde el Administrador de Django

--- dibujo/admin.py
+++ dibujo/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Dibujo)
+class DibujoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'autor', 'genero', 'stock', 'precio']
+    exclude = ('autor',)  # Oculta el campo 'autor' en el admin
+
+    def save_model(self, request, obj, form, change):
+        if not obj.autor_id:  # Si el autor no está definido, se asigna al usuario actual
+            obj.autor = request.user
+        obj.save()
+
+admin.site.register(Dibujo, DibujoAdmin)
 admin.site.register(Precio)
